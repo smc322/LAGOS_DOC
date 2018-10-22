1a54dfa4639256f8b1743ab70f2c4454a4c2a10d
Sarah Collins <smc322@cornell.edu>
2018-10-22 08:55:37 -0600

    reorganize files

--- Code/slopes plus explanatory vars data.R
+++ Code/slopes plus explanatory vars data.R
@@ -27,25 +27,40 @@ so4dat<-so4dep.llid[,c("lagoslakeid", "so4changepct")]
 
 chgpptso4<-merge(chgppt, so4dat, by="lagoslakeid")
 
+map.ndep.mat<-lagos$hu12.chag[,c("hu12_zoneid", "hu12_dep_no3_2000_mean", "hu12_runoff_mean", "hu12_prism_ppt_30yr_normal_800mm2_annual_mean", "hu12_prism_tmean_30yr_normal_800mm2_annual_mean", "hu12_baseflowindex_mean")]
+
+chag.llid<-merge(map.ndep.mat, hu12.llid, by="hu12_zoneid")
+
+chag.chgs<-merge(chgpptso4, chag.llid, by="lagoslakeid", all.x=T, all.y=F)
+
 cordsids<-unique(lagos$locus[,c("lagoslakeid", "nhd_lat", "nhd_long")])
 
-chgpptso4cords<-merge(chgpptso4, cordsids, by="lagoslakeid", all.x=T, all.y=F)
+chgpptso4cords<-merge(chag.chgs, cordsids, by="lagoslakeid", all.x=T, all.y=F)
 
 
 #wetlands = 90 and 95
 
 lulc<-lagos$iws.lulc
 lulc$wetlands<- lulc$iws_nlcd2006_pct_90+lulc$iws_nlcd2006_pct_95
-wtl<-lulc[,c("lagoslakeid", "wetlands")]
+lulc$agri<- lulc$iws_nlcd2006_pct_81 +lulc$iws_nlcd2006_pct_82
+lulc$urban<- lulc$iws_nlcd2006_pct_22 + lulc$iws_nlcd2006_pct_23 + lulc$iws_nlcd2006_pct_24
+
+  
+lulc.sums<-lulc[,c("lagoslakeid", "wetlands", "agri", "urban")]
 
 
-chagpptso4cordswtl<-merge(chgpptso4cords, wtl, by="lagoslakeid", all.x=T, all.y=F)
+chagpptso4cordswtl<-merge(chgpptso4cords, lulc.sums, by="lagoslakeid", all.x=T, all.y=F)
 
 
 depth<-lagos$lakes_limno[,c("lagoslakeid", "maxdepth")]
 
+area<-lagos$locus[,c("lagoslakeid", "lake_area_ha")]
+
 
 chagpptso4cordswtldepth<-merge(chagpptso4cordswtl, depth, by="lagoslakeid", all.x=T, all.y=F)
 
+all.covars<-merge(chagpptso4cordswtldepth, area, by="lagoslakeid", all.x=T, all.y=F)
 
-write.csv(chagpptso4cordswtldepth, file="Datasets/dataandcovarsJF.csv")
+all.covars$hu12_zoneid=NULL
+
+write.csv(all.covars, file="Datasets/dataandcovarsJF_17Sept.csv")
